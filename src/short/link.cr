e286875987ad55c466d447c69d8bd6ffee1a149d
Zac Nowicki <zachnowicki@gmail.com>
2017-08-13 14:13:43 -0400

    document Link

--- src/short/link.cr
+++ src/short/link.cr
@@ -2,11 +2,19 @@ require "http/client"
 require "openssl/ssl/context"
 
 module Short
+  # For performing requests with TLS
   SSL_CONTEXT = OpenSSL::SSL::Context::Client.new
 
+  # Maximum TTL on created `Link` objects
+  # TODO: Make this configurable.
   MAX_TTL = 30.days.to_i
+
+  # Minimum TTL on created `Link` objects
+  # TODO: Make this configurable.
   MIN_TTL = 60
 
+  # Converter for parsing JSON URL values into a `URI`
+  # for performing validation
   module URIConverter
     def self.from_json(parser)
       URI.parse parser.read_string
@@ -17,12 +25,16 @@ module Short
     end
   end
 
+  # Exception for Invalid URIs
   class InvalidURI < Exception
   end
 
+  # Exception for Invalid TTL values
   class InvalidTTL < Exception
   end
 
+  # A redirectable link that is validated against certain criteria,
+  # and an expiry TTL (the TTL must be implemented by a `Cache`)
   class Link
     JSON.mapping({
       target:     {type: URI, converter: URIConverter},
@@ -38,10 +50,17 @@ module Short
       validate!
     end
 
+    # Increase the amount of times this `Link` has been used.
+    # Mostly only useful for use with the `Memory` cache
     def use
       @uses += 1
     end
 
+    # Validates this `Link` instance
+    # Links must:
+    #   - Be of HTTPS scheme
+    #   - Be within MAX_TTL and MIN_TTL
+    #   - The target URI must respond to a HEAD request with a `200 OK` status code
     def validate!
       raise InvalidURI.new("Only HTTPS schemes are accepted.") unless target.scheme == "https"
       raise InvalidTTL.new("TTL greater than MAX_TTL: #{MAX_TTL}") if ttl > MAX_TTL
